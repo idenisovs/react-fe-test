b6e9d6202cd0f337321a6f258b4eb24bd29d97b9
Ilja Denisovs <i.denisovs@gmail.com>
2023-02-08 15:05:32 +0200

    Done basic Vine Card UI.

--- src/components/VineCard/index.tsx
+++ src/components/VineCard/index.tsx
@@ -7,10 +7,26 @@ export function VineCard(props: VineCardProps) {
 	const { vine } = props;
 
 	return (
-		<div className="VineCard">
-			<div>Vine Card</div>
-			<div>{vine.name}</div>
-			<div>{vine.type}</div>
-		</div>
+		<section className="VineCard">
+			<header>
+				<h2 className="VineCard--Label">
+					{vine.name}
+				</h2>
+				<small className="VineCard--Type">
+					{vine.type}
+				</small>
+			</header>
+			<main className="VineCard--Picture">
+				<img src="./vino-1.1.png" alt="vino-1.1.png" />
+			</main>
+			<footer className="VineCard--Actions">
+				<span>
+					Learn 〉
+				</span>
+				<span>
+					Shop 〉
+				</span>
+			</footer>
+		</section>
 	);
 }
--- src/components/VineSlider/index.tsx
+++ src/components/VineSlider/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Slider, { Settings } from 'react-slick';
 
 import { VineSliderProps } from './VineSliderProps';
 import { VineCard } from '../VineCard';
@@ -8,9 +9,20 @@ import './VineSlider.css';
 export function VineSlider(props: VineSliderProps) {
 	const { vines } = props;
 
+	const sliderSettings: Settings = {
+		dots: true,
+		lazyLoad: 'progressive',
+		infinite: true,
+		speed: 500,
+		slidesToShow: vines.length < 4 ? vines.length : 4,
+		slidesToScroll: 3
+	};
+
 	return (
 		<div className="VineSlider">
-			{vines.map((vine) => <VineCard key={vine.id} vine={vine} />)}
+			<Slider {...sliderSettings}>
+				{vines.map((vine) => <VineCard key={vine.id} vine={vine} />)}
+			</Slider>
 		</div>
 	);
 }
